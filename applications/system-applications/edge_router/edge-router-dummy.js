39cabaf5cf3172425e72c08bdee7876b1e4f675b
Jonathan Kelly <jonk@bigpond.com>
2017-11-29 14:31:09 +1100

    Adding dummy router for testing purposes.

---
+++ applications/system-applications/edge_router/edge-router-dummy.js
@@ -0,0 +1,101 @@
+#!/usr/bin/node
+
+// this application simulates data from edge router
+//
+
+//var brokerAddress = '192.168.0.30';
+var brokerAddress = '10.1.1.61';
+//var brokerAddress = '0.0.0.0';
+var brokerPort = 8888;
+
+var nodes = [
+        'affe:dead:beef::123',
+        'affe:dead:beef::124',
+        'affe:dead:beef::125',
+        'affe:dead:beef::126'
+]
+
+var quats = [];
+
+var glq = require('gl-quat');
+
+var dgram = require('dgram');
+sendAdvert();
+
+//process.exit();
+
+var c = 0;
+setInterval(function() {
+        calcQuats();
+        for (var i = 0; i < nodes.length; i++) {
+                sendOrientation(nodes[i], c);
+        }
+        if (c++ % 100000 === 0) {
+                sendAdvert();
+        }
+        if (c++ % 10000 === 0) {
+                sendStatus(nodes[i]);
+        }
+        if (c % 5000 === 0) {
+                sendCalibration(nodes[i]);
+        }
+}, 150);
+
+function calcQuats() {
+    for (var i = 0; i < nodes.length; i++) {
+        let addr = nodes[i];
+        let qIndex = nodes[i + 1];
+        if (qIndex === undefined) {
+            qIndex = nodes[0];
+        }
+        let prevQ = quats[qIndex];
+        if (prevQ === undefined) {
+            console.log('making', qIndex);
+            prevQ = glq.create();
+        } else {
+
+            var dx = (0.4 - Math.random()) / 500;
+            var dy = (0.4 - Math.random()) / 300;
+            var dz = (0.4 - Math.random()) / 10;
+            var dw = (0.4 - Math.random()) / 40;
+            prevQ[0] += dx + 0.2;
+            prevQ[1] += dy + 0.2;
+            prevQ[2] += dz;
+            prevQ[3] += dw;
+        }
+        glq.normalize(prevQ, prevQ);
+        quats[addr] = prevQ
+    }
+}
+
+function sendOrientation(addr, time) {
+        let q = quats[addr];
+        sendToBroker('do#' + time + ':' +
+                q[0] + ':' +
+                q[1] + ':' +
+                q[2] + ':' +
+                q[3] + ':' +
+                '#' + addr);
+}
+
+function sendStatus(addr) {
+        sendToBroker('ds#1:1:1#' + addr);
+}
+
+function sendCalibration(addr) {
+        sendToBroker('dc#-100:-100:-100:100:100:100:0#' + addr);
+}
+
+function sendAdvert() {
+    sendToBroker('biot er on port ' + brokerPort)
+}
+
+
+function sendToBroker(msg) {
+        var client = dgram.createSocket('udp4')
+        var message = new Buffer(msg);
+        client.send(message, 0, message.length, brokerPort, brokerAddress, function(err, bytes) {
+                if (err) throw err;
+                client.close();
+        });
+}
